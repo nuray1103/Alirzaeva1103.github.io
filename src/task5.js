23a3d430dc22af7338357ba43a1b4414f3926dbc
alirzaeva1103o <git@github.com:nuray1103/Alirzaeva1103.github.io.git>
2023-10-13 14:34:02 +0300

    Задание 5 выполнина

---
+++ src/task5.js
@@ -0,0 +1,6 @@
+const a = 'Jesse Pinkman: "Yo, Mr. White! I\'ve got a surprise for you!"\n';
+const b ='Walter White: "What is it?"\n';
+const c ='Jesse Pinkman: "It\'s a giant magnet!"\n';
+const d ='Walter White: "What the hell are we going to do with a giant magnet?"\n';
+
+console.log(`${a}${b}${c}${d}`);
