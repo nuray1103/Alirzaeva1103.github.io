c64c6375ffa3338b91e754ca529ccd1a33f11d61
alirzaeva1103o <git@github.com:nuray1103/Alirzaeva1103.github.io.git>
2023-10-13 14:54:39 +0300

    Задание 6.1(я просто забыла сожранить) выполнина

---
+++ src/task6.js
@@ -0,0 +1,7 @@
+const fuelConsumption = 10; //Расход топлива
+const distance = 100; //Дистанция
+const priceFuel = 1000; //Стоимость
+
+const result = (distance / fuelConsumption) * priceFuel;
+
+console.log(result)
